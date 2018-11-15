0c33364bb9bf052510e76dfd35a894bd43800379
Robin Han <33820379+robinhanstuy@users.noreply.github.com>
2018-11-15 10:12:54 -0500

    Update func.c

--- func.c
+++ func.c
@@ -7,8 +7,8 @@
 char **parse(char *cmd) {
     char *copy = calloc(strlen(cmd), sizeof(char));
     strcpy(copy, cmd);
-    char **arr = calloc(6, sizeof(char *));
-    for (int i = 0; copy; ++i){
+    char **arr = calloc(10, sizeof(char *));
+    for (int i = 0; copy; i++){
     	arr[i] = strsep(&copy, " ");
     }
     return arr;
